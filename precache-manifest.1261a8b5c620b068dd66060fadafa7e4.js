a2ad7ceae259408d856e2c04290fd71a4158b291
Danya_komlev <komlevdanila742@gmail.com>
2020-09-14 22:41:52 +0300

    Updates

---
+++ precache-manifest.1261a8b5c620b068dd66060fadafa7e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bd2b832c7fba8d0b78dd9ecb31fbc98",
+    "url": "/VKVezdecode/index.html"
+  },
+  {
+    "revision": "8314f07855529e2aa07a",
+    "url": "/VKVezdecode/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "8314f07855529e2aa07a",
+    "url": "/VKVezdecode/static/js/2.80973e7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/VKVezdecode/static/js/2.80973e7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "accb0f641c54b907913e",
+    "url": "/VKVezdecode/static/js/main.8d8bc3f0.chunk.js"
+  },
+  {
+    "revision": "ac27e8e4a579904f1c1d",
+    "url": "/VKVezdecode/static/js/runtime-main.2627366d.js"
+  }
+]);
